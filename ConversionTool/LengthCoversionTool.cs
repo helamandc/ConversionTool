1b4d6528197c940427367fe9d6856000f7bcf678
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2023-01-30 23:07:29 +1100

    Add project files.

---
+++ ConversionTool/LengthCoversionTool.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionTool
+{
+    public class LengthCoversionTool
+    {
+        //Convert feet to meter
+        //https://techstudy.org/csharp/Csharp-Program-to-print-convert-feet-to-meter/
+        [Test]
+        public void ToolConverter()
+        {
+            //Arrange
+            double feetLength = 50.00;
+            double meterLength;
+            double constant = 0.3048;
+            //Act
+            meterLength = constant * feetLength;
+            //Assert
+            Assert.AreEqual(15.24, meterLength, "Please check formula. Wrong conversion is calculated!");
+        }
+
+
+    }
+}
